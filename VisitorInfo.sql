ccdf2544f7b829b0275645ec6abedc86b8572d28
B Gabbard <Sm0k3scr33n@users.noreply.github.com>
2016-02-12 12:12:15 -0700

    Create VisitorInfo.sql

---
+++ VisitorInfo.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `tracker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(50) DEFAULT NULL,
+  `hostname` varchar(100) DEFAULT NULL,
+  `continent_code` varchar(4) DEFAULT NULL,
+  `country_code` varchar(4) DEFAULT NULL,
+  `country_code3` varchar(5) DEFAULT NULL,
+  `country_name` varchar(20) DEFAULT NULL,
+  `region` varchar(20) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  `postal_code` int(15) DEFAULT NULL,
+  `latitude` varchar(30) DEFAULT NULL,
+  `longitude` varchar(30) DEFAULT NULL,
+  `dma_code` varchar(20) DEFAULT NULL,
+  `area_code` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
