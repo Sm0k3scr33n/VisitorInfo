4a22ebdb355cba01f66d2c7aca12dd25a8f7a333
B Gabbard <Sm0k3scr33n@users.noreply.github.com>
2016-02-12 12:07:52 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+/*
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////+------------------------------------------------------------------------+///////////////
+////////////////|This script offers a way to get and store information about http clients|///////////////
+////////////////|to a MySQL databae.                                                     |///////////////
+////////////////|                                                                        |///////////////
+////////////////+------------------------------------------------------------------------+///////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+*/
+<?php
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {//get the http client ip
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+
+$hostname = gethostbyaddr($ip);/// get hostname
+
+printf( "the hostname is: $hostname");
+
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}else{echo "database connected";}
+
+mysql_select_db('VisitorInfo');
+
+$sql = "INSERT INTO tracker ".
+       "(id,ip,hostname,continent_code,country_code,country_code3,country_name,region,".
+       "city,postal_code,latitude,longitude,dma_code,area_code) ".
+       "VALUES ".
+       "('','$ip','$hostname','$line[continent_code]','$line[country_code]','$line[country_code3]',".
+	   "'$line[country_name]','$line[region]','$line[city]','$line[postal_code]','$line[latitude]',".
+	   "'$line[longitude]','$line[dma_code]','$line[area_code]')";
+
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not enter data: ' . mysql_error());
+}
+echo "Entered data successfully\n";
+mysql_close($conn);
+
+?>
